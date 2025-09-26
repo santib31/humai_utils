e4f569347bfdbacec60b8d1a7194e02e87cae1a6
Santi <santiagoboccardo31@gmail.com>
2025-09-26 09:50:41 -0300

    Mi primer paquete en Python

---
+++ DESCRIPTION.md
@@ -0,0 +1,1 @@
+Este es un paquete con muchas funciones comunes que encontramos en nuestros proyectos y que queremos compartir con la comunidad.
---
+++ humai_utils/functions.py
@@ -0,0 +1,7 @@
+import requests
+
+def saludar_comunidad():
+	print('Hola comunidad de Humai')
+
+def informar_clase():
+	print('La siguiente clase es el lunes')
